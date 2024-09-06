41ec85884f6d1c25226365d2a47f0a5c39c31f85
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2024-09-06 13:27:14 +0200

    Implemented SQL task 1b

--- task_1_b.sql
+++ task_1_b.sql
@@ -1,14 +1,13 @@
 -- 1.Task
 -- Get numbers of bikes sold in Jan and Feb 2020 per each vehicle make.
--- a) Display only those makes where total sales are more than 1000 units
+-- b) Display full sales data including all makes (including those where sales are not made)
 
 SELECT T_VEHICLE_MAKE.*, COUNT(T_LOAN.id) AS sold_count
 FROM `loan-schema`.`m_loan` AS T_LOAN
   JOIN `asset-schema`.`asset` AS T_ASSET ON T_LOAN.id = T_ASSET.m_loan_id
+    AND T_LOAN.disbursedon_date >= '2020-01-01'
+    AND T_LOAN.disbursedon_date < '2021-03-01' -- T_LOAN.disbursedon_date <= '2020-02-29' -- 02-28 or 02-29 depends on the year
   JOIN `asset-schema`.`vehicle_model` AS T_VEHICLE_MODEL ON T_ASSET.model_id = T_VEHICLE_MODEL.id
-  JOIN `asset-schema`.`vehicle_make` AS T_VEHICLE_MAKE ON T_VEHICLE_MODEL.vehicle_make_id = T_VEHICLE_MAKE.id
-WHERE T_LOAN.disbursedon_date >= '2020-01-01'
-  AND T_LOAN.disbursedon_date < '2020-03-01' -- T_LOAN.disbursedon_date <= '2020-02-29' -- 02-28 or 02-29 depends on the year
+  RIGHT JOIN `asset-schema`.`vehicle_make` AS T_VEHICLE_MAKE ON T_VEHICLE_MODEL.vehicle_make_id = T_VEHICLE_MAKE.id
 GROUP BY T_VEHICLE_MAKE.id
-HAVING sold_count > 1000
 ORDER BY sold_count DESC
